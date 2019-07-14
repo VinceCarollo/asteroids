d9f245381cb4fbd76eaac7e3e2e7068fcef2c46d
Vince Carollo <vincecarollo@gmail.com>
2019-07-14 13:28:51 -0600

    Refactor most dangerous day index action

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,8 +1,5 @@
 class MostDangerousDayController < ApplicationController
   def index
-    start_date = params[:start_date]
-    end_date = params[:end_date]
-    range_length = (end_date.to_datetime - start_date.to_datetime).to_s.split('/').first.to_i
     if range_length < 7
         render locals: {
           facade: MostDangerousDayFacade.new(start_date, end_date)
@@ -11,5 +8,19 @@ class MostDangerousDayController < ApplicationController
         flash.notice = 'Please enter dates with a max 7 day difference'
         redirect_to root_path
       end
+  end
+
+  private
+
+  def range_length
+    (end_date.to_datetime - start_date.to_datetime).to_s.split('/').first.to_i
+  end
+
+  def start_date
+    params[:start_date]
+  end
+
+  def end_date
+    params[:end_date]
   end
 end
