3c271bc30dc689fba1f1aa7acaaeeef229eef0ef
Vince Carollo <vincecarollo@gmail.com>
2019-07-14 13:40:26 -0600

    Refactor nasa service

--- app/services/nasa_service.rb
+++ app/services/nasa_service.rb
@@ -1,15 +1,22 @@
 class NasaService
   def asteroids_by_day_data(start_date, end_date)
-    conn = Faraday.new(url: 'https://api.nasa.gov') do |faraday|
-      faraday.adapter Faraday.default_adapter
-      faraday.params['api_key'] = ENV['NASA_KEY']
-    end
+    response = astroids_by_date(start_date, end_date)
+    JSON.parse(response.body, symbolize_names: true)[:near_earth_objects]
+  end
 
-    response = conn.get('/neo/rest/v1/feed') do |req|
+  private
+
+  def astroids_by_date(start_date, end_date)
+    conn.get('/neo/rest/v1/feed') do |req|
       req.params['start_date'] = start_date
       req.params['end_date'] = end_date
     end
+  end
 
-    JSON.parse(response.body, symbolize_names: true)[:near_earth_objects]
+  def conn
+    Faraday.new(url: 'https://api.nasa.gov') do |faraday|
+      faraday.adapter Faraday.default_adapter
+      faraday.params['api_key'] = ENV['NASA_KEY']
+    end
   end
 end
